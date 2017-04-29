5892cd9aeace25b25d5eaaa7ea26bb732339941a
Bud Parr <budparr@gmail.com>
2017-04-29 16:59:55 -0400

    add babel for users you may need it

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -8,6 +8,17 @@ module.exports = {
 	module: {
 		rules: [
 			{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['env']
+          // plugins: [require('babel-plugin-transform-object-rest-spread')]
+        }
+      }
+    },
+			{
 				test: /\.css$/,
 				use: ExtractTextPlugin.extract({
 					fallback: "style-loader",
