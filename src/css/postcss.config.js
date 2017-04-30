86d965bef53ee861eaa779b4ca76c4efd1a4a685
Bud Parr <budparr@gmail.com>
2017-04-29 22:48:41 -0400

    clean up

--- src/css/postcss.config.js
+++ src/css/postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
   plugins: {
    'postcss-import': {},
    'postcss-cssnext': {
-	     browsers: ['last 2 versions', '> 5%'],
-	     }
+	   browsers: ['last 2 versions', '> 5%'],
+	  }
 	}
 };
